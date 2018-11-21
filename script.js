ac97bf88735a7ee78ed5abdc156652cedba9e108
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2018-11-21 14:47:01 -0700

    Added color change functionality to pixel art grid

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+var brushColor;
 
-//creates a column:
+var pixel1 = document.getElementById()
+
+document.getElementById("red-paint").addEventListener("click", function(){
+    brushColor = "red"
+    pixel.classList.toggle(brushColor)
+    pixel.classList.toggle("pixel")
+
+})
+
+document.getElementById("orange-paint").addEventListener("click", function(){
+    brushColor = "orange"
+})
+
+document.getElementById("yellow-paint").addEventListener("click", function(){
+    brushColor = "yellow"
+})
+
+document.getElementById("green-paint").addEventListener("click", function(){
+    brushColor = "green"
+})
+
+document.getElementById("blue-paint").addEventListener("click", function(){
+    brushColor = "blue"
+})
+
+//creates grid of white pixels on screen:
 var body = document.getElementById("grid")
 for (var i = 0; i < 16; i++) {
     var row = document.createElement("div")
@@ -7,57 +33,95 @@ for (var i = 0; i < 16; i++) {
     for (var j = 0; j < 16; j++) {
         var pixel = document.createElement("div")
         pixel.classList.add("pixel")
+        // pixel. for loop through pixel to add individual ids to each;
+        //var x = document.getElementsByClassName("anchors")[0].id;
         row.appendChild(pixel)
-        
-        
+        pixel.addEventListener("click", function(){
+            event.target.className = brushColor
+    
+            //replace class with color of brush using brushColor
+            //class remove
+        })
 }
     body.appendChild(row)
 }
 
-//creat row, then fill row
 
-// for (var j = 0; i < 16; j++) {
-//     var body = document.getElementById("grid")
-//     var canvas = document.createElement("div")
-//     canvas.classList.add("canvas")
+
+
+// //THIS CODE WORKS!:
+// var brushColor;
+
+// var pixel1 = document.getElementById()
+
+// document.getElementById("red-paint").addEventListener("click", function(){
+//     brushColor = "red"
+//     pixel.classList.toggle(brushColor)
+//     pixel.classList.toggle("pixel")
+
+// })
+
+// document.getElementById("orange-paint").addEventListener("click", function(){
+//     brushColor = "orange"
+// })
+
+// document.getElementById("yellow-paint").addEventListener("click", function(){
+//     brushColor = "yellow"
+// })
+
+// document.getElementById("green-paint").addEventListener("click", function(){
+//     brushColor = "green"
+// })
+
+// document.getElementById("blue-paint").addEventListener("click", function(){
+//     brushColor = "blue"
+// })
+
+// //creates grid of white pixels on screen:
+// var body = document.getElementById("grid")
+// for (var i = 0; i < 16; i++) {
 //     var row = document.createElement("div")
 //     row.classList.add("row")
-//     var pixel = document.createElement("div")
-//     pixel.classList.add("pixel")
-
-//     body.appendChild(canvas)
-//     canvas.appendChild(row)
-//     row.appendChild(pixel)
+//     for (var j = 0; j < 16; j++) {
+//         var pixel = document.createElement("div")
+//         pixel.classList.add("pixel")
+//         // pixel. for loop through pixel to add individual ids to each;
+//         //var x = document.getElementsByClassName("anchors")[0].id;
+//         row.appendChild(pixel)
+//         pixel.addEventListener("click", function(){
+//             event.target.className = brushColor
+    
+//             //replace class with color of brush using brushColor
+//             //class remove
+//         })
 // }
-
-//
-
-//
-//need to isolate each part 
-//if i click on blue, 
-//then click on pixel, 
-//I want the pixel background color to turn blue
-
-
-
-
-
-
-//     card.appendChild(image)
-//     card.appendChild(cardBody)
-//     cardBody.appendChild(cardTitle)
-//     cardBody.appendChild(cardText)
-//     card.appendChild(unorderedList)
-//     unorderedList.appendChild(listItem)
-//     body.appendChild(card)
-
-
+//     body.appendChild(row)
 // }
-//document.addEventListener('DOMContentLoaded', function() {
-//     console.log('I was loaded')
-//     var pixelElement = document.getElementsByClassName("pixel")
-//     pixelElement[0].addEventListener("click", function(event){
-//         console.log("clicked")
-//         pixelElement[0].className = "blue-paint"
-//     })
+//event handler to each color on pallette and then wherever click, assigns that color to pixel
+
+
+
+//turns first pixel red when click on it:
+// var pixel = document.getElementsByClassName("pixel")[0]
+// pixel.addEventListener("click", function(e){
+//     pixel = pixel.id = "red"
 // })
+
+// var body = document.getElementById("grid")
+// for (var i = 0; i < 16; i++) {
+//     var row = document.createElement("div")
+//     row.classList.add("row")
+//     for (var j = 0; j < 16; j++) {
+//         var pixel = document.createElement("div")
+//         pixel.classList.add("pixel")
+//         row.appendChild(pixel)
+//         //if clear class to add color, need to re add pixel
+//         //in css, can select canvas ID and then write div after 
+//         //instead of using specific pixel class - any div that 
+//         //is a child of the canvas ID
+        
+// }
+//     body.appendChild(row)
+// }
+
+// document.getElementsByClassName("pixel")[0].id = "blue-paint"
