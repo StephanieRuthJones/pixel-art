c38e7b178b769962d8f02ff84e091d12e3c04e91
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2018-11-21 15:35:54 -0700

    Completed project with full functionality

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var brushColor;
 
-var pixel1 = document.getElementById()
+
+
+// //THIS CODE WORKS!:
+//for each event listener need if statement which changes color on 
+//first click and changes back to white on second
+var brushColor;
 
 document.getElementById("red-paint").addEventListener("click", function(){
     brushColor = "red"
-    pixel.classList.toggle(brushColor)
-    pixel.classList.toggle("pixel")
-
 })
 
 document.getElementById("orange-paint").addEventListener("click", function(){
@@ -37,7 +38,11 @@ for (var i = 0; i < 16; i++) {
         //var x = document.getElementsByClassName("anchors")[0].id;
         row.appendChild(pixel)
         pixel.addEventListener("click", function(){
-            event.target.className = brushColor
+            if (event.target.className === brushColor) {
+                event.target.className = "pixel"
+            } else {
+                event.target.className = brushColor
+            }
     
             //replace class with color of brush using brushColor
             //class remove
@@ -45,58 +50,6 @@ for (var i = 0; i < 16; i++) {
 }
     body.appendChild(row)
 }
-
-
-
-
-// //THIS CODE WORKS!:
-// var brushColor;
-
-// var pixel1 = document.getElementById()
-
-// document.getElementById("red-paint").addEventListener("click", function(){
-//     brushColor = "red"
-//     pixel.classList.toggle(brushColor)
-//     pixel.classList.toggle("pixel")
-
-// })
-
-// document.getElementById("orange-paint").addEventListener("click", function(){
-//     brushColor = "orange"
-// })
-
-// document.getElementById("yellow-paint").addEventListener("click", function(){
-//     brushColor = "yellow"
-// })
-
-// document.getElementById("green-paint").addEventListener("click", function(){
-//     brushColor = "green"
-// })
-
-// document.getElementById("blue-paint").addEventListener("click", function(){
-//     brushColor = "blue"
-// })
-
-// //creates grid of white pixels on screen:
-// var body = document.getElementById("grid")
-// for (var i = 0; i < 16; i++) {
-//     var row = document.createElement("div")
-//     row.classList.add("row")
-//     for (var j = 0; j < 16; j++) {
-//         var pixel = document.createElement("div")
-//         pixel.classList.add("pixel")
-//         // pixel. for loop through pixel to add individual ids to each;
-//         //var x = document.getElementsByClassName("anchors")[0].id;
-//         row.appendChild(pixel)
-//         pixel.addEventListener("click", function(){
-//             event.target.className = brushColor
-    
-//             //replace class with color of brush using brushColor
-//             //class remove
-//         })
-// }
-//     body.appendChild(row)
-// }
 //event handler to each color on pallette and then wherever click, assigns that color to pixel
 
 
